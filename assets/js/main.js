4ed4ff6e42383ea838e5d431c966aa8aebaf9af7
Alp Niksarlı <alp.niksarli@gmail.com>
2023-09-20 01:31:23 -0400

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,20 @@
+let imgs = document.querySelectorAll("img");
+let leftbull = document.getElementById("leftbull"); 
+let sun = document.getElementById("sun");
+let rightbull = document.getElementById("rightbull"); 
 
-/* javascript */
+sun.addEventListener("mouseover", e => {
+    leftbull.style.transform = `rotate(-45deg)`;
+    rightbull.style.transform = `rotate(45deg)`;
+
+    // leftbull.style.webkitTransform = `translateX(100px)`;
+    // rightbull.style.webkitTransform = `translateX(-100px)`;
+})
+
+sun.addEventListener("mouseleave", e => {
+    leftbull.style.transform = `rotate(0deg)`;
+    rightbull.style.transform = `rotate(0deg)`;
+
+    leftbull.style.transform = `translateX(100px)`;
+    rightbull.style.transform = `translateX(-100px)`;
+})
